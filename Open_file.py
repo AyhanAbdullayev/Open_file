4af161c5c06b1649d000a722b9b6650f989afbfc
AyhanAbdullayev <ayhanabdullayev4@gmail.com>
2024-07-29 12:06:57 +0400

    Open_filev1

---
+++ Open_file.py
@@ -0,0 +1,36 @@
+from datetime import date 
+from os.path import exists
+
+if not exists("table.txt"):
+    with open("table.txt","w") as f:
+        f.write("Function name   |Worked Time|Arguments|Keyword Arguments|Result\n")
+
+def calculation(a):
+    def inner(*args,**kwargs):
+        try:
+            result = a(*args,**kwargs)
+            result = str(result)
+        except ZeroDivisionError as Z:
+            result = str(Z)
+        a_name = f"{a.__name__:<16}"
+        Worked_time = f"{date.today()}"
+        Args = ",".join(map(str,args))
+        Kwargs = ",".join([f"{key}={value}" for key,value in kwargs.items()])
+        with open("table.txt","a") as f:
+            f.write(f"{a_name}| {Worked_time}|  {Args}   |       {Kwargs}          |{result}\n")
+    return inner
+
+@calculation
+
+def sum(a,b):
+    return a+b
+
+@calculation
+
+
+def divide(a,b):
+    return a/b
+
+
+sum(10,1)
+divide(a=4,b=3)
